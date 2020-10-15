bb299a93761daa935558f091bce58d5e85e15495
Platholl <cyprien.plateauholleville@gmail.com>
2020-10-15 22:16:04 +0200

    Rename WatchPrinter to make it more polyvalent.

--- Assets/Scripts/PlayerStuff/HideControler.cs
+++ Assets/Scripts/PlayerStuff/HideControler.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WatchPrinter : MonoBehaviour
+public class HideControler : MonoBehaviour
 {
-    public GameObject printerPrefab;
-
+    public GameObject toHideControlPrefab;
+    public string buttonName = "Action 1";
+    
     private GameObject m_instantiatedPrefab;
     private bool m_isHidden = false;
     
         
     void Start()
     {
-        m_instantiatedPrefab = Instantiate(printerPrefab, new Vector3(0, 0, 0), printerPrefab.transform.rotation);
+        m_instantiatedPrefab = Instantiate(toHideControlPrefab, new Vector3(0, 0, 0), toHideControlPrefab.transform.rotation);
         SetEnableChildrensMeshRenderer(m_isHidden);
         m_instantiatedPrefab.transform.SetParent(gameObject.transform);
         m_instantiatedPrefab.transform.localPosition = new Vector3(0, 0.01f, 0);
@@ -20,9 +21,8 @@ public class WatchPrinter : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetButtonDown("Action 1"))
+        if (Input.GetButtonDown(buttonName))    
         {
-            Debug.Log("value " + m_isHidden.ToString());
             m_isHidden = !m_isHidden;
             SetEnableChildrensMeshRenderer(m_isHidden);
         }
